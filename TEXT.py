49fd867dc4104636c25d0a32b62d1d746e695a6a
BigChoCho <a3858117@gmail.com>
2018-11-11 18:36:34 +0800

    Initial commit

---
+++ TEXT.py
@@ -0,0 +1,20 @@
+#定義一個類(學生)
+class Student():
+    pass
+#一個對象繼承一個類
+ciomion =Student()
+
+#定義一個愛打手槍的學生類
+class FapStuden():
+    #用None給不確定者賦值
+    name =None
+    age =15
+    course = "觀摩A片"
+    #函數(行為)
+    def Fap( self):
+        print("我愛打雞雞")
+
+        return None
+yuyue = FapStuden()
+#對象執行函數
+yuyue.Fap()
