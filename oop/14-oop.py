b70212ef17ca6db611208c3a666cdd677f894429
BigChoCho <a3858117@gmail.com>
2018-12-01 16:32:53 +0800

    幣值轉換

--- oop/14-oop.py
+++ oop/14-oop.py
@@ -1,57 +1,10 @@
-# Mixin案例
-#多態(單一類的多功能運用)
-class Person():
-    name = "liuying"
-    age = 18
-
-    def eat(self):
-        print("EAT.......")
-
-    def drink(self):
-        print("DRINK......")
-
-    def sleep(self):
-        print("SLEEP.....")
-
-
-class Teacher(Person):
-    def work(self):
-        print("Work")
-
-
-class Student(Person):
-    def study(self):
-        print("Study")
-
-
-class Tutor(Teacher, Student):
+class a():
     pass
-
-
-t = Tutor()
-
-print(Tutor.__mro__)
-print(t.__dict__)
-print(Tutor.__dict__)
-
-print("*" * 20)
-
-
-class TeacherMixin():
-    def work(self):
-        print("Work")
-
-
-class StudentMixin():
-    def study(self):
-        print("Study")
-
-
-class TutorM(Person, TeacherMixin, StudentMixin):
+class b(a):
     pass
-
-#助教繼承老師與學生的功能
-tt = TutorM()
-print(TutorM.__mro__)
-print(tt.__dict__)
-print(TutorM.__dict__)
+class c():
+    pass
+#判斷是否為某子類(子類,父類)的繼承關係
+print(issubclass(b,a))
+print(issubclass(c,b))
+print(issubclass(b,object))
